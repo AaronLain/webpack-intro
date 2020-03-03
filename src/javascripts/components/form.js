7356d909bc177850d971de40532026d40f91d1fd
Aaron Lain <aaron.j.lain@gmail.com>
2020-03-02 21:24:24 -0600

    no errors??

---
+++ src/javascripts/components/form.js
@@ -0,0 +1,23 @@
+import util from '../helpers/utils';
+import display from './display';
+import textData from '../helpers/data/textData';
+
+const submitForm = (e) => {
+  e.preventDefault();
+  const formText = $('#form-text').val();
+  textData.setText(formText);
+  display.displayForm();
+};
+const displayForm = () => {
+  let domString = '';
+  domString += '<form>';
+  domString += '<div class="form-group">';
+  domString += '<label for="form-text">Text</label>';
+  domString += '<input type="text" class="form-control" id="form-text">';
+  domString += '</div>';
+  domString += '<button type="submit" class="btn btn-primary">Submit</button>';
+  domString += '</form>';
+  util.printToDom('form-container', domString);
+};
+
+export default { displayForm, submitForm };
